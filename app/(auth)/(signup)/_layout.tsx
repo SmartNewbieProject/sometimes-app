bda2eb3ce3a5fa5a34d2d4aed742e262420dcb11
deveungi <deveungi@gmail.com>
2025-04-12 09:43:53 +0900

    feat: 백 핸들링 추가

--- app/(auth)/(signup)/_layout.tsx
+++ app/(auth)/(signup)/_layout.tsx
@@ -1,28 +1,47 @@
-import { useSignupProgress } from '@/src/features/signup/hooks';
 import { cn } from '@/src/shared/libs/cn';
 import { platform } from '@/src/shared/libs/platform';
 import { PalePurpleGradient } from '@/src/shared/ui/gradient';
 import { ProgressBar } from '@/src/shared/ui/progress-bar';
-import { Stack } from 'expo-router';
+import { Stack, router } from 'expo-router';
 import { View } from 'react-native';
+import Signup from '@features/signup';
+import { useCallback, useRef } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { BackHandler } from 'react-native';
+
+const { useSignupProgress, SignupSteps } = Signup;
 
 export default function SignupLayout() {
-  const { progress } = useSignupProgress();
-  
+  const { progress, updateStep } = useSignupProgress();
+
+  const handleBackPress = useCallback(() => {
+    updateStep(SignupSteps.TERMS);
+    return true;
+  }, [updateStep]);
+
+  useFocusEffect(
+    useCallback(() => {
+      BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+      return () => {
+        BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+      };
+    }, [handleBackPress])
+  );
+
   return (
     <View className="flex-1">
-        <PalePurpleGradient />
-        <View className={cn(
-          "px-5 pb-[30px] items-center bg-white",
-          platform({
-            ios: () => "pt-[80px]",
-            android: () => "pt-[80px]",
-            web: () => "pt-[30px]",
-          })
-        )}>
-          <ProgressBar progress={progress} />
-        </View>
-        <Stack
+      <PalePurpleGradient />
+      <View className={cn(
+        "px-5 pb-[30px] items-center bg-white",
+        platform({
+          ios: () => "pt-[80px]",
+          android: () => "pt-[80px]",
+          web: () => "pt-[30px]",
+        })
+      )}>
+        <ProgressBar progress={progress} />
+      </View>
+      <Stack
         screenOptions={{
           headerShown: false,
           contentStyle: {
@@ -32,8 +51,13 @@ export default function SignupLayout() {
         }}
       >
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="email" options={{ headerShown: false }} />
-        <Stack.Screen name="password" options={{ headerShown: false }} />
+        <Stack.Screen 
+          name="account" 
+          options={{ 
+            headerShown: false,
+            headerBackVisible: false,
+          }}
+        />
         <Stack.Screen name="profile" options={{ headerShown: false }} />
       </Stack>
     </View>
