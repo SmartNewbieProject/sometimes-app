75d1346cf19a1eb63ba9cbf70f73e9e32169b8e5
deveungi <deveungi@gmail.com>
2025-04-12 11:37:11 +0900

    style: adjust login page style

--- src/features/signup/ui/login-form.tsx
+++ src/features/signup/ui/login-form.tsx
@@ -2,30 +2,45 @@ import { Input } from "@/src/shared/ui/input";
 import { View } from "react-native";
 import { Text } from "@/src/shared/ui";
 import SignupButtons from "./buttons";
+import { Form } from "@/src/widgets";
+import { useForm } from "react-hook-form";
+
+type Form = {
+  email: string;
+  password: string;
+}
 
 export default function LoginForm() {
+  const form = useForm<Form>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
   return (
     <View className="flex flex-col flex-1 h-full">
-      <View className="flex flex-col gap-y-[40px] mt-8">
-        <View className="flex flex-col gap-y-2">
-          <Text className="text-[18px] font-semibold text-primaryPurple">이메일</Text>
-          <Input
+      <View className="flex flex-col gap-y-[20px] mt-8">
+          <Form.LabelInput
+            name="email"
+            control={form.control}
+            label="이메일"
+            size="sm"
             placeholder="이메일 주소"
           />
-        </View>
-
-          <View>
-          <Text className="text-[18px] font-semibold text-primaryPurple">비밀번호</Text>
-          <Input
+          <Form.LabelInput
+            name="password"
+            control={form.control}
+            label="비밀번호"
+            size="sm"
             placeholder="영문, 숫자, 특수문자 조합 8자리 이상"
             isPassword
           />
-        </View>
 
         <View className="flex-row justify-center items-center gap-x-2">
-          <Text className="text-gray-600">아이디 찾기</Text>
-          <Text className="text-gray-300">|</Text>
-          <Text className="text-gray-600">비밀번호 찾기</Text>
+          <Text className="text-gray-600 text-sm">아이디 찾기</Text>
+          <Text className="text-gray-300 text-sm">|</Text>
+          <Text className="text-gray-600 text-sm">비밀번호 찾기</Text>
         </View>
       </View>
       <SignupButtons />
