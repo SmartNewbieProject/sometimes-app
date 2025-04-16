c2d7a22d7a5394aa915b8285ba2847d6d102e3c3
galaxy4276 <deveungi@gmail.com>
2025-04-16 22:24:34 +0900

    refactor: image-selector -> content-selector 분리

--- src/widgets/form/index.tsx
+++ src/widgets/form/index.tsx
@@ -2,10 +2,15 @@ import { FormImageSelector } from "./image-selector";
 import { FormInput } from "./input";
 import { FormLabelInput } from "./label-input";
 import { FormSelect } from "./select";
+import { FormContentSelector } from "./content-selector";
+import { FormColorSelector, FormTextSelector } from "./examples";
 
 export const Form = {
   Input: FormInput,
   LabelInput: FormLabelInput,
   ImageSelector: FormImageSelector,
+  ContentSelector: FormContentSelector,
+  ColorSelector: FormColorSelector,
+  TextSelector: FormTextSelector,
   Select: FormSelect,
 }
