ffa164384a96aeab496cd2dae0326376dbad53b4
galaxy4276 <deveungi@gmail.com>
2025-04-18 21:01:16 +0900

    feat: 관심사 기능 추가

--- src/features/interest/index.tsx
+++ src/features/interest/index.tsx
@@ -1,11 +1,15 @@
 import * as hooks from './hooks';
 import * as UI from './ui';
 import * as services from './services';
+import * as apis from './api';
+import * as queries from './queries';
 
 const Interest = {
   hooks,
   ui: UI,
   services,
+  apis,
+  queries,
 };
 
 export default Interest;
