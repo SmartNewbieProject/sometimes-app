ffebc5988955d5b47aeb4be265e34bcf5b614a9d
minesp3164 <lhd000721@gmail.com>
2025-04-18 15:34:36 +0900

    feat: 알림 UI 스타일링

---
+++ src/features/mypage/ui/custom-switch.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native';
+
+const CustomSwitch = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitch = () => setIsOn(!isOn);
+
+  return (
+    <TouchableOpacity onPress={toggleSwitch} style={styles.switchContainer}>
+      <View
+        style={[
+          styles.switch,
+          isOn ? styles.switchOn : styles.switchOff,
+        ]}
+      >
+        <View
+          style={[
+            styles.switchButton,
+            isOn ? styles.switchButtonOn : styles.switchButtonOff,
+          ]}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  switchContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  switch: {
+    width: 60,
+    height: 30,
+    borderRadius: 20,
+    padding: 3,
+    
+    flexDirection: 'row',
+    shadowColor: '#00000040',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+  },
+  switchOn: {
+    backgroundColor: '#F3EDFF',
+  },
+  switchOff: {
+    backgroundColor: '#F3EDFF',
+  },
+  switchButton: {
+    shadowColor: '#00000040',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: '#7A4AE2',
+    position: 'absolute',
+    top: 3,
+    left: 3,
+    transition: 'all 0.3s ease', // 부드러운 애니메이션
+  },
+  switchButtonOn: {
+    left: 33, // 스위치 오른쪽으로 이동
+  },
+  switchButtonOff: {
+    left: 3, // 스위치 왼쪽으로 이동    
+  },
+
+});
+
+export default CustomSwitch;
--- src/features/mypage/ui/notice.tsx
+++ src/features/mypage/ui/notice.tsx
@@ -1,39 +1,26 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity, Switch } from 'react-native';
+import { View } from 'react-native';
 import { Text } from '@/src/shared/ui';
-
+import CustomSwitch from './custom-switch';
 
 export const Notice = () => {
     const [isMatchComplete, setIsMatchComplete] = useState(false);
     const [isEvent, setIsEvent] = useState(false);
 
     return (
-        <View style={{ padding: 16 }}>
+        <View>
             <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 8 }}>알림 설정</Text>
             <View style={{ borderBottomWidth: 1, borderBottomColor: '#E0E0E0', marginBottom: 16 }} />
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 16 }}>
                 <Text style={{ fontSize: 16, color: '#757575' }}>매칭 완료 알림</Text>
-                <TouchableOpacity>
-                    <Switch 
-                        value={isMatchComplete} 
-                        className='w-[66px] h-[32px] thumb-[#7A4AE2]'
-                        onValueChange={() => setIsMatchComplete(!isMatchComplete)} 
-                    />
-                </TouchableOpacity>
+                <CustomSwitch value={isMatchComplete} onChange={setIsMatchComplete} />
             </View>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Text style={{ fontSize: 16, color: '#757575' }}>이벤트 알림</Text>
-                <TouchableOpacity onPress={() => setIsEvent(!isEvent)}>
-                    <Switch
-                        value={isEvent}
-                        onValueChange={() => setIsEvent(!isEvent)}
-                        thumbColor={isEvent ? '#7c3aed' : '#f4f3f4'}
-                        trackColor={{ false: '#a0d5d3', true: '#e9d5ff' }}
-                        ios_backgroundColor="#ccc"
-                    />
-                </TouchableOpacity>
+                <CustomSwitch value={isEvent} onChange={setIsEvent} />  
             </View>
         </View>
     );
 }
 
+export default Notice;
