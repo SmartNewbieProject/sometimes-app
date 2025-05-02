02e09f9390e6fc47bcdc5c490297199743692a22
Smartnewb <smartnewb2@gmail.com>
2025-05-02 14:09:00 +0900

    fix: 사업자 정보 위치 수정

--- app/event/pre-signup.tsx
+++ app/event/pre-signup.tsx
@@ -146,7 +146,7 @@ export default function PreSignupScreen() {
         </Button>
 
         {/* 회사 정보 푸터 */}
-        <View className="w-full px-4 py-6 mt-8 bg-[#F8F9FA] border-t border-gray-200">
+        <View className="w-full px-4 py-4 mt-6 sticky bottom-0 left-0">
           <Text className="text-[#888] text-[10px] text-center leading-4">
             상호명: 스마트 뉴비 | 사업장 소재지: 대전광역시 유성구 동서대로 125, S9동 202호 | 대표: 전준영 | 사업자 등록번호: 498-05-02914 | 통신판매업신고: 제 2025-대전유성-0530호 | 문의전화: 010-8465-2476 | 이메일: notify@smartnewb.com | 사업자정보
           </Text>
