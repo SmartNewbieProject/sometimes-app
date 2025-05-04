795302ff33d2c388dfca74a5bc52a1915ea255b6
galaxy4276 <deveungi@gmail.com>
2025-05-04 17:35:49 +0900

    feat: 게시글 작성 기능

---
+++ src/features/community/services/article-writer.tsx
@@ -0,0 +1,15 @@
+import { FormProvider } from "react-hook-form";
+import { useArticleWriteForm } from "../hooks/article-writer";
+
+type FormProviderProps = {
+  children: React.ReactNode;
+  form: ReturnType<typeof useArticleWriteForm>;
+}
+
+export const ArticleWriteFormProvider = ({ children, form }: FormProviderProps) => {
+  return (
+    <FormProvider {...form}>
+      {children}
+    </FormProvider>
+  );
+};
