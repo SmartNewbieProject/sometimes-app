202ee9aaaea35770ec69b22a12c7c5b4f6b4f9c7
galaxy4276 <deveungi@gmail.com>
2025-05-08 10:24:48 +0900

    refactor: 드롭다운 리팩터링

--- app/community/[id].tsx
+++ app/community/[id].tsx
@@ -4,12 +4,11 @@ import { TouchableOpacity, View, Pressable } from "react-native";
 import { ArticleDetail } from "@/src/features/community/ui/article-detail/article-detail";
 import ChevronLeftIcon from '@/assets/icons/chevron-left.svg';
 import { useArticleDetail } from "@/src/features/community/hooks/";
-import { useForm } from "react-hook-form";
-import { ImageResources } from "@/src/shared/libs";
 import { useAuth } from "@/src/features/auth";
 import { useBoolean } from "@/src/shared/hooks/use-boolean";
 import apis from "@/src/features/community/apis";
 import Loading from "@/src/features/loading";
+import { Dropdown } from '@/src/shared/ui/dropdown';
 
 export default function ArticleDetailScreen() {
     const { id } = useLocalSearchParams<{ id: string }>();
@@ -45,52 +44,23 @@ export default function ArticleDetailScreen() {
                 </Header.LeftContent>
                 <Header.Logo title="커뮤니티" showLogo={true} logoSize={128} />
                 <Header.RightContent>
-                    <TouchableOpacity>
-											{/* TODO: 신고기능 추후 연동 */}
-                        {/* <Show when={!isOwner}>
-                            <View className="">
-                                <Text>신고</Text>
-                            </View>
-                        </Show> */}
-                        <Show when={isOwner}>
-                            <View className="" onTouchEnd={(e) => {
-                                e.stopPropagation();
-                            }}>
-                                <TouchableOpacity onPress={(e) => {
-                                    e.stopPropagation();
-                                    toggleDropdown();
-                                }}>
-                                    <View className="w-[48px] h-[48px] flex items-center justify-center">
-                                        <ImageResource resource={ImageResources.MENU} width={24} height={24} />
-                                    </View>
-                                </TouchableOpacity>
-                            </View>
-                        </Show>
-                        <Show when={isDropdownOpen}>
-                            <View style={dropdownStyles.dropdownContainer}>
-                                <TouchableOpacity
-                                    style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#f0f0f0' }}
-                                    onPress={(e) => {
-                                        e.stopPropagation();
-                                        closeDropdown();
-                                        router.push(`/community/update/${id}`);
-                                    }}
-                                >
-                                    <Text textColor="black">수정</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity
-                                    style={{ padding: 10 }}
-                                    onPress={(e) => {
-                                        e.stopPropagation();
-                                        handleDelete();
-                                        closeDropdown();
-                                    }}
-                                >
-                                    <Text textColor="black">삭제</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </Show>
-                    </TouchableOpacity>
+                    <Show when={isOwner}>
+                        <Dropdown
+                            open={isDropdownOpen}
+                            items={[
+															{
+																key: 'edit',
+																content: <Text textColor="black">수정</Text>,
+																onPress: () => router.push(`/community/update/${id}`),
+															},
+															{
+																key: 'delete',
+																content: <Text textColor="black">삭제</Text>,
+																onPress: handleDelete,
+															},
+                            ]}
+                        />
+                    </Show>
                 </Header.RightContent>
 
             </Header.Container>
