305d6ee29d4bfd542f69fe1e99a9cea405f9a58e
minesp3164 <lhd000721@gmail.com>
2025-05-17 20:19:54 +0900

    feat: SOM-28 #done 외부 피드 리스트 댓글 너비가 초과하는 오류 해결

--- src/features/community/ui/comment/index.tsx
+++ src/features/community/ui/comment/index.tsx
@@ -14,27 +14,34 @@ export const Comment = ({ data, className }: CommentProps) => {
   const author = data?.author;
 
   return (
-    <View className={`w-full flex flex-row ${className}`}>
-      <View className="flex-row items-center mb-2">
+    <View className={`w-full flex-row ${className}`}>
+      <View className="mr-2">
         <Image
           source={{ uri: imageUri }}
           style={{ width: 36, height: 36 }}
           onError={err => {
             setImageUri(getUnivLogo(UniversityName.한밭대학교));
           }}
-          className="rounded-full mr-2"
+          className="rounded-full"
         />
       </View>
-      <View className="flex flex-col gap-y-1">
-        <View className="flex flex-row gap-x-1.5">
-          <Text size="13" textColor="black">{author.name}</Text>
-          <Text size="sm" className="text-[#646464]">{dayUtils.formatRelativeTime(data.updatedAt)}</Text>
-        </View>
 
-        <Text size="sm" textColor="black">
-          {data.content}
-        </Text>
+        <View className="flex-1 flex-col gap-y-1">
+          <View className=" flex-row gap-x-1.5 w-full">
+            <Text size="13" textColor="black">{author.name}</Text>
+            <Text size="sm" className="text-[#646464]">{dayUtils.formatRelativeTime(data.updatedAt)}</Text>
+          </View>
+          <View className="pt-1.5">
+            <Text
+            size="sm"
+            textColor="black"
+            style={{ flexShrink: 1 }}
+            >
+                {data.content}
+          </Text>
+        </View>
       </View>
     </View>
+    
   );
 };
