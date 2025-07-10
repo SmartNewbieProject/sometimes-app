f958f392d82632492c2321495f5283ca8980facc
galaxy4276 <deveungi@gmail.com>
2025-07-10 16:30:39 +0900

    feat: 애플 로그인 API 추가

--- src/features/auth/apis/index.tsx
+++ src/features/auth/apis/index.tsx
@@ -1,14 +1,16 @@
-import type { MyDetails } from "@/src/types/user";
-import { axiosClient } from "@shared/libs";
-import type { PassLoginResponse } from "@/src/auth/dto/pass-login.dto";
+import {axiosClient} from "@shared/libs";
+import type {MyDetails} from "@types/user";
+import {PassLoginResponse} from "@/src/auth/dto/pass-login.dto";
+
+export {appleReviewLogin} from "./apple-review-login";
 
 export const getMySimpleDetails = () => axiosClient.get('/user');
 
 export const checkExistsInstagram = (instagramId: string): Promise<{ exists: boolean }> =>
-  axiosClient.post('/auth/check/instagram', { instagramId });
+    axiosClient.post('/auth/check/instagram', {instagramId});
 
 export const getMyDetails = (): Promise<MyDetails> =>
-  axiosClient.get('/user/details');
+    axiosClient.get('/user/details');
 
 export const passLogin = (impUid: string): Promise<PassLoginResponse> =>
-  axiosClient.post('/auth/pass-login', { impUid });
+    axiosClient.post('/auth/pass-login', {impUid});
