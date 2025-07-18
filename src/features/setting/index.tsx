9d90166e24992d2d087280e9d14e298ea03bac34
mayrang <pkss0626@naver.com>
2025-07-18 15:39:01 +0900

    Fix: 설정 페이지 추가

---
+++ src/features/setting/index.tsx
@@ -0,0 +1,10 @@
+import { AccountManagementMenu, AccountMenu, Menu, SettingHeader } from "./ui";
+
+const Setting = {
+  SettingHeader,
+  AccountManagementMenu,
+  AccountMenu,
+  Menu,
+};
+
+export default Setting;
