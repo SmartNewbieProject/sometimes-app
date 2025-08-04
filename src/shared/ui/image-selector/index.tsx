ba9f1c1a4543637947c19b7daf45e9e9e1a760c6
mayrang <pkss0626@naver.com>
2025-08-04 17:14:52 +0900

    hotfix: 프로필 수정 불가 해결

--- src/shared/ui/image-selector/index.tsx
+++ src/shared/ui/image-selector/index.tsx
@@ -79,19 +79,6 @@ export function ImageSelector({
     });
     console.log("image result", result);
     if (!result.canceled) {
-      const type = result.assets[0].type;
-      const extension = type?.split(".").pop()?.toLowerCase();
-
-      const allowedExtensions = ["jpg", "jpeg", "png"];
-      if (!extension || !allowedExtensions.includes(extension)) {
-        Alert.alert(
-          "지원하지 않는 형식",
-          "jpg, jpeg, png 형식만 업로드 가능해요"
-        );
-        setImageModal(false);
-        return null;
-      }
-
       onChange(result.assets[0].uri);
     }
     setImageModal(false);
@@ -122,19 +109,6 @@ export function ImageSelector({
     }
 
     if (!result.canceled) {
-      const type = result.assets[0].type;
-      const extension = type?.split(".").pop()?.toLowerCase();
-
-      const allowedExtensions = ["jpg", "jpeg", "png"];
-      if (!extension || !allowedExtensions.includes(extension)) {
-        Alert.alert(
-          "지원하지 않는 형식",
-          "jpg, jpeg, png 형식만 업로드 가능해요."
-        );
-        setImageModal(false);
-        return null;
-      }
-
       onChange(result.assets[0].uri);
     }
     setImageModal(false);
