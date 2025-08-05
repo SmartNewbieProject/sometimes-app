054f835ad2f8e3b292b1468d02ae8cd691e6e5c1
mayrang <pkss0626@naver.com>
2025-08-05 11:03:43 +0900

    Feat: 모달에 로고를 추가할 수 있도록 변경

--- app/test/index.tsx
+++ app/test/index.tsx
@@ -1,9 +1,18 @@
+import { useModal } from "@/src/shared/hooks/use-modal";
 import Slider from "@/src/widgets/slide";
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 
 function Test() {
-  return <Slider />;
+  const { showModal } = useModal();
+
+  useEffect(() => {
+    showModal({
+      showLogo: true,
+      title: "인증번호가 메일로 전송됐어요",
+      children: "메일을 확인해 주세요",
+    });
+  }, []);
 }
 
 const styles = StyleSheet.create({});
