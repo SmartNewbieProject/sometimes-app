d858840ada8a4627171bfceff2d3a036d6fc368b
galaxy4276 <deveungi@gmail.com>
2025-08-06 16:57:05 +0900

    feat: 기존 재매칭권 가이드 추가

--- src/features/payment/ui/gem-store/banner.tsx
+++ src/features/payment/ui/gem-store/banner.tsx
@@ -1,6 +1,6 @@
-import {ImageResources} from "@/src/shared/libs/image";
-import {ImageResource} from "@/src/shared/ui";
-import {Dimensions, View} from "react-native";
+import {ImageResources} from "@shared/libs/image";
+import {ImageResource, Text } from "@shared/ui";
+import {Dimensions, Pressable, StyleSheet, View} from "react-native";
 
 export const Banner = () => {
   const width = Dimensions.get("window").width;
@@ -12,11 +12,41 @@ export const Banner = () => {
   })();
 
   return (
-      <View>
+      <View style={styles.container}>
         <ImageResource
             resource={ImageResources.GEM_STORE_BANNER}
             style={{width: "100%", height}}
         />
+        <Pressable style={styles.guideButton}>
+          <ImageResource
+            resource={ImageResources.TICKET}
+            width={36}
+            height={36}
+          />
+          <Text weight="bold" textColor="black" size="12">
+            기존 재매칭권은 어떻게 되나요?
+          </Text>
+        </Pressable>
       </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: "relative",
+    width: "100%",
+  },
+  guideButton: {
+    backgroundColor: "white",
+    paddingHorizontal: 8,
+    width: 'auto',
+    borderRadius: 8,
+    position: "absolute",
+    top: 8,
+    right: 8,
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    columnGap: 4,
+  }
+});
