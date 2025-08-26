6e400b8636641f309177d15d5f9358dcbb500073
heychi <heychi0728@gmail.com>
2025-08-26 10:39:15 +0900

    fix: 홈 하단 사업자 정보에서 사업자 등록번호 링킹되도록 수정

--- src/shared/ui/business-info/business-info.tsx
+++ src/shared/ui/business-info/business-info.tsx
@@ -6,6 +6,9 @@ export const BusinessInfo: React.FC = () => {
     Linking.openURL(link);
   };
 
+  const businessRegistrationLink =
+    "https://www.ftc.go.kr/bizCommPop.do?wrkr_no=4980502914";
+
   return (
     <View
       className={`w-full flex flex-col items-center px-4 py-6 mt-4 ${
@@ -14,8 +17,16 @@ export const BusinessInfo: React.FC = () => {
     >
       <Text className="text-[#888] text-[10px] text-center leading-5">
         상호명: 스마트 뉴비 | 사업장 소재지: 대전광역시 서구 갈마중로 7번길 42,
-        4동 407호 | 대표: 전준영 | 사업자 등록번호: 498-05-02914 |
-        통신판매업신고: 제 2025-대전유성-0530호 | 문의전화: 010-8465-2476 |
+        4동 407호 | 대표: 전준영 |{" "}
+        <TouchableOpacity
+          onPress={() => onClickLink(businessRegistrationLink)}
+          className="underline inderline-offset-1"
+        >
+          <Text className="text-[#888] text-[10px]">
+            사업자 등록번호: 498-05-02914
+          </Text>
+        </TouchableOpacity>{" "}
+        | 통신판매업신고: 제 2025-대전유성-0530호 | 문의전화: 010-8465-2476 |
         이메일: notify@smartnewb.com | 사업자정보
       </Text>
       <View className="flex flex-row gap-x-2 items-center justify-center w-full">
