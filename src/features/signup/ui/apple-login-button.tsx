65c2bb97becf59a3497c87fb814ddb64ebfea0b6
mayrang <pkss0626@naver.com>
2025-08-26 15:02:39 +0900

    hotfix: 애플로그인 불필요한 alert 제거

--- src/features/signup/ui/apple-login-button.tsx
+++ src/features/signup/ui/apple-login-button.tsx
@@ -154,7 +154,6 @@ const AppleLoginButton: React.FC = () => {
       });
     } catch (error) {
       console.error("백엔드 요청 실패:", error);
-      alert("로그인에 실패했습니다. 다시 시도해주세요.");
     } finally {
       setIsLoading(false);
     }
