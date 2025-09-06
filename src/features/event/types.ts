78be273cf522ab458d8473385066e8d61ecceadb
eungi <deveungi@gmail.com>
2025-09-07 04:09:09 +0900

    feat: 재화 변경 프로세스 수정 (#147)
    * feat: 재화 변경 프로세스 수정
    
    * refactor: 상수 관리형태로 변경, 이벤트 훅 내용 수정
    
    * feat: 할인 이벤트
    
    * fix: 이벤트 중복참여안되게 수정
    
    * refactor: 상수 분리
    
    * fix: 타이포 버그 수정

--- src/features/event/types.ts
+++ src/features/event/types.ts
@@ -1,8 +1,8 @@
 export enum EventType {
   FIRST_SALE = 'FIRST_SALE',
-  FIRST_SALE_6 = 'FIRST_SALE_6',
-  FIRST_SALE_20 = 'FIRST_SALE_20',
-  FIRST_SALE_40 = 'FIRST_SALE_40',
+  FIRST_SALE_7 = 'FIRST_SALE_7',
+  FIRST_SALE_16 = 'FIRST_SALE_16',
+  FIRST_SALE_27 = 'FIRST_SALE_27',
 }
 
 export type EventDetails = {
