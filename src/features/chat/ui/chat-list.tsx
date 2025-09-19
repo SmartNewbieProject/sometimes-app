ee31b68611ab735bc8b07ce56d4afade562308f8
mayrang <pkss0626@naver.com>
2025-09-20 02:33:19 +0900

    Fix: 시간 관련 수정

--- src/features/chat/ui/chat-list.tsx
+++ src/features/chat/ui/chat-list.tsx
@@ -56,27 +56,27 @@ const ChatList = ({ setPhotoClicked }: ChatListProps) => {
 
   const chatList = data?.pages.flatMap((page) => page.messages) ?? [];
 
-  const formattedChatList = chatList.map((chat) => {
-    const date = new Date(chat.createdAt);
-    date.setHours(date.getHours() - 9);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    return { ...chat, createdAt: formattedDate };
-  });
+  // const formattedChatList = chatList.map((chat) => {
+  //   const date = new Date(chat.createdAt);
+  //   date.setHours(date.getHours() - 9);
+  //   const year = date.getFullYear();
+  //   const month = String(date.getMonth() + 1).padStart(2, "0");
+  //   const day = String(date.getDate()).padStart(2, "0");
+  //   const hours = String(date.getHours()).padStart(2, "0");
+  //   const minutes = String(date.getMinutes()).padStart(2, "0");
+  //   const seconds = String(date.getSeconds()).padStart(2, "0");
+  //   const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  //   return { ...chat, createdAt: formattedDate };
+  // });
 
   const sortedChatList = useMemo(() => {
-    return [...formattedChatList]
+    return [...chatList]
       .sort(
         (a, b) =>
           new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
       )
       .reverse();
-  }, [formattedChatList]);
+  }, [JSON.stringify(chatList)]);
 
   const chatListWithDateDividers = useMemo(() => {
     const items: ChatListItem[] = [];
