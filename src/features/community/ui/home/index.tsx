39b93798a56fd81bad86bfcae43c5fccfc69346d
heychi <heychi0728@gmail.com>
2025-10-22 23:25:40 +0900

    fix: 커뮤니티 홈 스크롤 되도록 수정

--- src/features/community/ui/home/index.tsx
+++ src/features/community/ui/home/index.tsx
@@ -1,6 +1,13 @@
 import { PalePurpleGradient } from "@/src/shared/ui";
 import React from "react";
-import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Linking,
+  ScrollView,
+} from "react-native";
 import { IconWrapper } from "@/src/shared/ui/icons";
 import VectorIcon from "@/assets/icons/Vector.svg";
 import NotiPreView from "@/src/features/community/ui/home/notice";
@@ -18,73 +25,80 @@ export default function CommuHome() {
     <View className="flex-1">
       <PalePurpleGradient />
 
-      <View className="px-4 pt-3">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 24 }}
+      >
+        <View className="px-4 pt-3">
+          <TouchableOpacity
+            className="flex-row items-center"
+            onPress={() => changeCategory(NOTICE_CODE)}
+            accessibilityRole="button"
+          >
+            <Image
+              source={require("@/assets/images/loudspeaker.png")}
+              style={{ width: 26, height: 26 }}
+            />
+            <Text className="text-[24px] font-semibold mt-2 mx-2">
+              공지사항
+            </Text>
+            <View className="ml-auto">
+              <IconWrapper>
+                <VectorIcon className="h-[24px] w-[9px] mx-2" color="black" />
+              </IconWrapper>
+            </View>
+          </TouchableOpacity>
+        </View>
+
         <TouchableOpacity
-          className="flex-row items-center"
-          onPress={() => changeCategory(NOTICE_CODE)}
-          accessibilityRole="button"
+          onPress={() =>
+            Linking.openURL(
+              "https://ruby-composer-6d2.notion.site/FAQ-1ff1bbec5ba1803bab5cfbe635bba220?source=copy_link"
+            )
+          }
+          className="bg-[#F3EDFF] rounded-[5px] mx-[16px] px-4 py-2 my-[10px] gap-x-2 flex-row items-center"
+          activeOpacity={0.8}
         >
           <Image
             source={require("@/assets/images/loudspeaker.png")}
-            style={{ width: 26, height: 26 }}
+            style={{ width: 16, height: 16 }}
           />
-          <Text className="text-[24px] font-semibold mt-2 mx-2">공지사항</Text>
+          <Text className="text-[14px]">[FAQ] 자주묻는 질문</Text>
+
           <View className="ml-auto">
             <IconWrapper>
-              <VectorIcon className="h-[24px] w-[9px] mx-2" color="black" />
+              <VectorIcon className="h-[12px] w-[9px]" color="black" />
             </IconWrapper>
           </View>
         </TouchableOpacity>
-      </View>
 
-      <TouchableOpacity
-        onPress={() =>
-          Linking.openURL(
-            "https://ruby-composer-6d2.notion.site/FAQ-1ff1bbec5ba1803bab5cfbe635bba220?source=copy_link"
-          )
-        }
-        className="bg-[#F3EDFF] rounded-[5px] mx-[16px] px-4 py-2 my-[10px] gap-x-2 flex-row items-center"
-        activeOpacity={0.8}
-      >
-        <Image
-          source={require("@/assets/images/loudspeaker.png")}
-          style={{ width: 16, height: 16 }}
-        />
-        <Text className="text-[14px]">[FAQ] 자주묻는 질문</Text>
-
-        <View className="ml-auto">
-          <IconWrapper>
-            <VectorIcon className="h-[12px] w-[9px]" color="black" />
-          </IconWrapper>
+        <View className="mx-[16px] mb-[16px]">
+          <NotiPreView pageSize={5} />
         </View>
-      </TouchableOpacity>
 
-      <View className="mx-[16px] mb-[16px]">
-        <NotiPreView pageSize={5} />
-      </View>
+        <View className="px-4 py-3">
+          <TouchableOpacity
+            className="flex-row items-center"
+            onPress={() => changeCategory(HOT_CODE)}
+            accessibilityRole="button"
+          >
+            <Image
+              source={require("@/assets/images/fireIcon.png")}
+              style={{ width: 26, height: 26 }}
+            />
+            <Text className="text-[24px] font-semibold mt-2 mx-2">인기</Text>
+            <View className="ml-auto">
+              <IconWrapper>
+                <VectorIcon className="h-[24px] w-[9px] mx-2" color="black" />
+              </IconWrapper>
+            </View>
+          </TouchableOpacity>
+        </View>
 
-      <View className="px-4 py-3">
-        <TouchableOpacity
-          className="flex-row items-center"
-          onPress={() => changeCategory(HOT_CODE)}
-          accessibilityRole="button"
-        >
-          <Image
-            source={require("@/assets/images/fireIcon.png")}
-            style={{ width: 26, height: 26 }}
-          />
-          <Text className="text-[24px] font-semibold mt-2 mx-2">인기</Text>
-          <View className="ml-auto">
-            <IconWrapper>
-              <VectorIcon className="h-[24px] w-[9px] mx-2" color="black" />
-            </IconWrapper>
-          </View>
-        </TouchableOpacity>
-      </View>
-
-      <View className="mx-[2px] mb-[2px]">
-        <HotPreView pageSize={5} />
-      </View>
+        <View className="mx-[2px] mb-[2px]">
+          <HotPreView pageSize={5} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
