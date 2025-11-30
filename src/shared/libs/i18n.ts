abee0896fc781e4f31a558518edd9f2e69ac2a71
minesp3164 <lhd000721@gmail.com>
2025-11-30 17:42:57 +0900

    fix: 일본어가 나오기 전 한글 한번 로드되는경우 해결

--- src/shared/libs/i18n.ts
+++ src/shared/libs/i18n.ts
@@ -1,34 +1,31 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { getUserLanguage } from './local';
+import * as Localization from 'expo-localization';
 import ko from './locales/ko';
 import ja from './locales/ja';
 
 const resources = {
-  ja: {
-    translation: ja,
-  },
-  ko: {
-    translation: ko,
-  },
+  ja: { translation: ja },
+  ko: { translation: ko },
 };
 
-const supportedLngs = ['ja', 'ko'];
-const userLang = getUserLanguage();
+const deviceLocales = Localization.getLocales();
+const deviceLang = deviceLocales[0]?.languageTag?.toLowerCase().startsWith('ja')
+  ? 'ja'
+  : 'ko';
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: supportedLngs.includes(userLang) ? userLang : 'ko',
-  fallbackLng: {
-    "ko-*:": ['ko'],
-    "ja-*:": ['ja'],
-    default: ['ko'],},
-  interpolation: {
-    escapeValue: false, 
-  },
-  react: {
-    useSuspense: false, 
-  },
-});
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: deviceLang,
+    fallbackLng: 'ko',
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
 
 export default i18n;
