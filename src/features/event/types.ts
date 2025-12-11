ac370e944cf3c297797d3656f2f5bc1e0f7f4dc4
YUJAEYUN <yujeayoon13@naver.com>
2025-12-12 01:27:23 +0900

    feat: 커뮤니티 인사글 작성시 구슬 지급기능 추가

--- src/features/event/types.ts
+++ src/features/event/types.ts
@@ -3,6 +3,7 @@ export enum EventType {
 	FIRST_SALE_7 = 'FIRST_SALE_7',
 	FIRST_SALE_16 = 'FIRST_SALE_16',
 	FIRST_SALE_27 = 'FIRST_SALE_27',
+	COMMUNITY_FIRST_POST = 'COMMUNITY_FIRST_POST',
 }
 
 export type EventDetails = {
