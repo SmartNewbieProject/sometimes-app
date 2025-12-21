78de2904a9609611d9087d058ab472ab6d190883
galaxy4276 <deveungi@gmail.com>
2025-12-21 17:29:56 +0900

    chore: 의존성 설치 및 스크립트 수정

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = (api) => {
 	return {
 		presets: ['babel-preset-expo'],
 		plugins: [
+			'babel-plugin-transform-import-meta',
 			[
 				'react-native-reanimated/plugin',
 				{
--- metro.config.js
+++ metro.config.js
@@ -7,12 +7,16 @@ const { transformer, resolver } = config;
 config.transformer = {
   ...transformer,
   babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  // import.meta 지원을 위한 설정
+  unstable_allowRequireContext: true,
 };
 
 config.resolver = {
   ...resolver,
   assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
   sourceExts: [...resolver.sourceExts, 'svg'],
+  // ESM 패키지 지원
+  unstable_enablePackageExports: true,
 };
 
 config.server = {
